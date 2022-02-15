5b30bb4e62f426ce49f26e7c0cc4b6c6c0411a5b
Alan Rente <alan.rente@iksolution.com.br>
2022-02-14 23:14:43 -0300

    routes get and post cartoes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CartoesController } from './cartoes/cartoes.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CartoesController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/cartoes/cartoes.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+type Cartao = {
+  id: number;
+  numero: string;
+  bandeira: string;
+  validade: string;
+};
+
+const _cartoes: Cartao[] = [
+  {
+    id: 1,
+    numero: '1234 5678 9012 3456',
+    bandeira: 'visa',
+    validade: '10/20',
+  },
+  {
+    id: 2,
+    numero: '1234 5678 9012 5050',
+    bandeira: 'mastercard',
+    validade: '10/25',
+  },
+];
+
+@Controller('cartoes')
+export class CartoesController {
+  @Get()
+  obterTodos(): Cartao[] {
+    return _cartoes;
+  }
+
+  @Get(':id')
+  obterPorId(@Param('id') id: number): Cartao {
+    return _cartoes.find((cartao) => cartao.id === id);
+  }
+  @Post()
+  criar(@Body() cartao: Cartao): Cartao {
+    return cartao;
+  }
+}

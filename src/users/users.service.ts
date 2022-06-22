4dbe519dc48ddcb7dba0551ca2553567968728f5
Alan Rente <alan.rente@iksolution.com.br>
2022-06-22 02:15:15 -0300

    Initial build Jwt

---
+++ src/users/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any;
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      username: 'admin',
+      password: 'admin',
+    },
+    {
+      id: 2,
+      username: 'user',
+      password: 'user',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}

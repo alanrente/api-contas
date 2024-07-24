6afb9426ed395b01fbbc9805a2874736c46c2fe9
Alan Rente <alan.rente@gmail.com>
2024-07-24 15:04:34 -0300

    acertando public

--- src/public/public.module.ts
+++ src/public/public.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PublicController } from './public.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PublicaEntity } from './entities/publica.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([PublicaEntity])],
   controllers: [PublicController],
   providers: [],
   exports: [],
